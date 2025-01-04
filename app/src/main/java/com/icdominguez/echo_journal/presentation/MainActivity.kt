09a625fda1bf3489676080e7efcf5316709b4df6
Ismael Cordón <icordondominguez@gmail.com>
2025-01-04 19:46:48 +0100

    [MVP-002] Splash screen

--- app/src/main/java/com/icdominguez/echo_journal/presentation/MainActivity.kt
+++ app/src/main/java/com/icdominguez/echo_journal/presentation/MainActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.icdominguez.echo_journal.presentation.navigation.Navigation
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.icdominguez.echo_journal.presentation.designsystem.theme.EchoJournalTheme
+import com.icdominguez.echo_journal.presentation.navigation.Navigation
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             EchoJournalTheme {
